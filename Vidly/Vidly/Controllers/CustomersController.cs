08f6fee9bc64e922511984413ef0816086753a4e
Md. Jahangir Alam <seeworld24@gmail.com>
2020-06-11 01:57:54 +0600

    FundamentalEnd

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetAllCustomer();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetAllCustomer().SingleOrDefault(r => r.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private List<Customer> GetAllCustomer()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Jahangir"},
+                new Customer {Id = 2, Name = "Rubel"}
+            };
+            return customers;
+        }
+    }
+}
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,24 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+        public ViewResult Index()
+        {
+            var movies = GetAllMovies();
+            return View(movies);
+        }
 
         public ActionResult Random()
         {
-            var movies = new Movie() {Name = "Shark!"};
-            return View(movies);
+            var movies = new Movie() { Name = "Shark!" };
+            var customer = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomViewModel
+            {
+                Movie = movies,
+                Customers = customer
+            };
+            return View(viewModel);
         }
+
+        private List<Movie> GetAllMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shark!"},
+                new Movie {Id = 2, Name = "Hulk"}
+            };
+            return movies;
+        }
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+
+        //    return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        //}
     }
 }
